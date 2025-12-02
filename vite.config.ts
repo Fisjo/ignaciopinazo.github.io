3e7fb14a9a88758e4ace65512b9ed14542df5d66
Fisjo <ignacio.po.ipo@gmail.com>
2025-12-02 17:54:45 +0100

    feat: New build to /docs for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/ignaciopinazo.github.io/',
+      base: '/ignaciopinazo.github.io/', // <--- RUTA BASE CORRECTA
       
       server: {
         port: 3000,
@@ -16,6 +16,10 @@ export default defineConfig(({ mode }) => {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
+      build: {
+        outDir: 'docs', 
+      },
+      // ----------------------
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
